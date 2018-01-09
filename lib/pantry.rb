8a50a84ff3a5ee6cc621e5653d7e7e16df23039f
Luis Garcia <garcia@yahoo.com>
2018-01-09 09:24:46 -0700

    Create test and method for stock check and restock

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,3 +1,24 @@
 class Pantry
+  attr_reader :stock
+
+  def initialize
+    @stock = {}
+  end
+
+  def stock_check(food)
+    if !@stock.has_key?(food)
+      0
+    else
+      @stock[food]
+    end
+  end
+
+  def restock(food, num)
+    if stock.has_key?(food)
+      stock[food] += num
+    else
+      stock[food] = num
+    end
+  end
 
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -3,5 +3,28 @@ require 'minitest/autorun'
 require 'minitest/pride'
 
 class PantryTest < Minitest::Test
+  attr_reader :pantry
+
+  def setup
+    @pantry = Pantry.new  
+  end
+
+  def test_it_exist
+    assert_instance_of Pantry, pantry
+  end
+
+  def test_pantry_begins_with_empty_hash
+    assert_equal ({}), pantry.stock
+  end
+
+  def test_if_stock_has_any_food
+    assert_equal 0, pantry.stock_check("Cheese")
+  end
+
+  def test_if_restock_adds_food
+    pantry.restock("Cheese", 10)
+    
+    assert_equal 10, pantry.stock_check("Cheese")
+  end
 
 end
